60e2232628f1ec9104e92fded7d9d4726beca2d1
gallayl <gallay.lajos@gmail.com>
2018-01-22 10:48:32 +0100

    [KFI]feat(Models): added IActionModel

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { IAuthenticationService } from "./Authentication/IAuthenticationService"
 export { LoginState } from "./Authentication/LoginState";
 export { ConstantContent } from "./Repository/ConstantContent";
 export { IContent } from "./Models/IContent";
+export { IActionModel } from "./Models/IActionModel";
